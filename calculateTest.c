45f7713dd3c88d09b55b262033fd8a002f574c6b
TranAlanD <alantran0118@gmail.com>
2025-10-18 00:31:45 -0400

    Added calculateTest.c

---
+++ calculateTest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int length = 4;
+int width = 6;
+int area;
+
+area = length * width;
+
+printf("length is: %d\n", length);
+printf("width is: %d\n", width);
+printf("area is: %d\n", area);
+}
