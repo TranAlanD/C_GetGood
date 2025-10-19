83966573bddecfcc1df2ad85f5ca254e1b22089c
TranAlanD <alantran0118@gmail.com>
2025-10-19 02:34:34 -0400

    added switches.c

---
+++ switches.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    int day = 7;
+
+    switch(day) {
+        case 1:
+            printf("Monday");
+        case 2:
+            printf("Tuesday");
+        case 3:
+            printf("Wednesday");
+        case 4:
+            printf("Thursday");
+        case 5:
+            printf("Friday");
+        case 6:
+            printf("Saturday");
+        case 7:
+            printf("Sunday");
+    }
+}
